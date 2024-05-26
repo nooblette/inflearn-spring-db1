8f4849dbc8f7a8a2a2e12324eccc4582bb8fe3fb
nooblette <min9hyuk@gmail.com>
2024-05-26 13:25:37 +0900

    feat : sec 02. 커넥션풀과 데이터소스 이해 - DataSource 예제2 - 커넥션 풀

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -11,6 +11,8 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -33,6 +35,20 @@ public class ConnectionTest {
 		// 하지만 애플리케이션에서 DataSource 인터페이스에만 의존할 수 있다는 이점이 있다. (DIP, OCP)
 		DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 		useDataSource(dataSource);
+	}
+
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		// Hikari를 사용한 Connection Pooling
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(10); // 커넥션 풀 최대 개수 지정 (default = 10)
+		dataSource.setPoolName("MyPool");
+
+		useDataSource(dataSource);
+		Thread.sleep(1000);
 	}
 
 	private void useDataSource(DataSource dataSource) throws SQLException {
